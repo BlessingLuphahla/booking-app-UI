18345ede06bcf72562f7028801e63d5395d8aaf2
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-12 19:21:46 +0200

    initial API integration

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,20 +1,31 @@
 import "./featured.css";
+import { useAPI } from "../../hooks/useAPI";
 import { feauturedList } from "../../data/featured";
 
 function Featured() {
+  const { data, loading, error } = useAPI(
+    "/hotels/countByCity?cities=berlin,madrid",
+    "GET"
+  );
+
+  console.log(data);
+
   return (
     <div className="featured">
-      {feauturedList.map((element, index) => {
-        return (
-          <div key={index} className="featuredItem">
-            <img className="featuredImg" src={element.src} alt="" />
-            <div className="featuredTitles">
-              <h1>{element.title1}</h1>
-              <h2>{element.title2}</h2>
-            </div>
-          </div>
-        );
-      })}
+      {error && <div>{error}</div>}
+      {!error && loading
+        ? "Loading, Please wait..."
+        : feauturedList.map((element, index) => {
+            return (
+              <div key={index} className="featuredItem">
+                <img className="featuredImg" src={element.src} alt="" />
+                <div className="featuredTitles">
+                  <h1>{element.title1}</h1>
+                  <h2>{data[index]} properties</h2>
+                </div>
+              </div>
+            );
+          })}
     </div>
   );
 }
---
+++ src/hooks/useAPI.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+export const useAPI = (apiEndpoint, method = "GET") => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        if (method === "GET") {
+          const res = await axios.get(
+            `${import.meta.env.VITE_API_URL}/api${apiEndpoint}`
+          );
+          setData(res.data);
+        } else if (method === "POST") {
+          const res = await axios.post(
+            `${import.meta.env.VITE_API_URL}/api${apiEndpoint}`
+          );
+          setData(res.data);
+        } else if (method === "DELETE") {
+          const res = await axios.delete(
+            `${import.meta.env.VITE_API_URL}/api${apiEndpoint}`
+          );
+          setData(res.data);
+        }
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [apiEndpoint, method]);
+
+  return { data, loading, error };
+};
