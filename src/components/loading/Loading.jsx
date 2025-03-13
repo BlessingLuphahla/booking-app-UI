8b928712c2792b1761d3d1dfab49cbe911fef6de
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-03-13 12:52:13 +0200

    Loading update

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./loading.css";
 
 function Loading() {
   return (
-    <div>Loading...</div>
-  )
+    <div className="loadingContainer">
+      <div className="spinner"></div>
+    </div>
+  );
 }
 
-export default Loading
+export default Loading;
